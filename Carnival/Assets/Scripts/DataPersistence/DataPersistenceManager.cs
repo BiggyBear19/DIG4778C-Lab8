eb602b61f2940aa2487a32472c04c83986f200b2
rickyje3 <122560145+rickyje3@users.noreply.github.com>
2024-10-27 15:43:47 -0400

    test

--- Carnival/Assets/Scripts/DataPersistence/DataPersistenceManager.cs
+++ Carnival/Assets/Scripts/DataPersistence/DataPersistenceManager.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class DataPersistenceManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameData gameData;
+
+    //Gets instance publicly but can only be edited it privately in this class
+    public static DataPersistenceManager instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (instance != null)
+        {
+            Debug.Log("Found more than one data persistence manager in scene");
+        }
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        LoadGame();
+    }
+
+    public void NewGame()
+    {
+        //Initialize gameData by making a new GameData object
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        //load saved data from a file using data handler
+
+        //if no data loaded, make a new game
+        if (this.gameData == null)
+        {
+            Debug.Log("No data was found. Initializing data to defaults");
+        }
+    }
+
+    public void SaveGame()
+    {
+        //pass data to other scripts to update
+        //save that data to a file using the data handler
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
     }
 }
